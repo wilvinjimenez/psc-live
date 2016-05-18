d7ef5920e5cd3ef2392e1265ae3dea9d84673924
unknown <wjimenez@WJIMENEZ.transunion.com.do>
2016-05-18 16:34:04 -0400

    Cookies and twitter widget width.

--- src/js/iys.psc.common.js
+++ src/js/iys.psc.common.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var common = (function (window, document) {
+var common = (function(window, document) {
 
     var commonObj = {};
 
-    commonObj.getTimestamp = function () {
+    commonObj.getTimestamp = function() {
 
         var currentdate = new Date(),
             timestamp = currentdate.getDate() + '' + (currentdate.getMonth() + 1) + '' + currentdate.getFullYear() + '' + currentdate.getHours() + '' + currentdate.getMinutes() + '' + currentdate.getSeconds();
@@ -13,14 +13,14 @@ var common = (function (window, document) {
 
     },
 
-        commonObj.getContextProtocol = function () {
+        commonObj.getContextProtocol = function() {
 
             var protocol = /^http:/.test(document.location) ? 'http' : 'https';
             return protocol;
 
         },
 
-        commonObj.loadScript = function (id, url) {
+        commonObj.loadScript = function(id, url) {
 
             var s = 'script',
                 js = document.createElement(s),
@@ -34,7 +34,7 @@ var common = (function (window, document) {
         },
 
         //Fisher–Yates Shuffle.
-        commonObj.shuffle = function (array) {
+        commonObj.shuffle = function(array) {
 
             var i = array.length,
                 j = 0,
@@ -55,47 +55,50 @@ var common = (function (window, document) {
 
         },
 
-        commonObj.setCookie = function (cname, cvalue, exdays) {
+        commonObj.setCookie = function(cName, cValue, exDate) {
 
-            var d = new Date();
-            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-
-            var expires = 'expires=' + d.toUTCString();
-            document.cookie = cname + '=' + cvalue + '; ' + expires;
+            var expiryDate = 'expires=' + new Date(exDate).toUTCString();
+            document.cookie = cName + '=' + cValue + '; ' + expiryDate + '; path=/';
 
         },
 
-        commonObj.getCookie = function (cname) {
+        commonObj.getCookie = function(cName) {
 
-            var name = cname + '=',
+            var name = cName + '=',
                 ca = document.cookie.split(';');
 
             for (var i = 0; i < ca.length; i++) {
+
                 var c = ca[i];
 
-                while (c.charAt(0) == ' ') {
+                while (c.charAt(0) == ' ')
                     c = c.substring(1);
-                }
 
-                if (c.indexOf(name) == 0) {
+                if (c.indexOf(name) == 0)
                     return c.substring(name.length, c.length);
-                }
+
             }
 
             return '';
 
+        },
+
+        commonObj.deleteCookie = function(cName) {
+
+            this.setCookie(cName, '', -1);
+
+        },
+
+        commonObj.probablyIsMobile = function() {
+
+            var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+                isACommonMobileOS = (window.navigator.userAgent.match(/iPhone|iPad|iPod|Android|webOS|BlackBerry|Windows Phone/i)) ? true : false;
+
+            if (viewportWidth <= 1280 && isACommonMobileOS) return true
+
+            return false;
+
         };
-
-    commonObj.probablyIsMobile = function () {
-
-        var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
-            isACommonMobileOS = (window.navigator.userAgent.match(/iPhone|iPad|iPod|Android|webOS|BlackBerry|Windows Phone/i)) ? true : false;
-
-        if (viewportWidth <= 1280 && isACommonMobileOS) return true
-
-        return false;
-
-    }
 
     return commonObj;
 
--- src/js/iys.psc.subscriptions.js
+++ src/js/iys.psc.subscriptions.js
@@ -24,7 +24,19 @@ var subscriptionsModule = (function (window, document) {
             $subscriptionModal.modal('hide');
 
         });
+        
+        var expiryDate = new Date();
+        expiryDate.setTime(expiryDate.getTime() + (24 * 60 * 60 * 1000));
+        expiryDate.setHours(8);
+        expiryDate.setMinutes(0);
+        expiryDate.setSeconds(0);
 
+        var expiryUTCDate = expiryDate.toUTCString();
+
+        var subscriptionCookie = 'WC_SC=CONFIRMED; expires=' + expiryUTCDate + '; path=/';
+        
+        expiryDate.setHours(expiryDate.getHours() + 1);        
+        
     };
 
     return subscriptionsModuleObj;
